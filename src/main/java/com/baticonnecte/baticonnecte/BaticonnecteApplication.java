fe7cff0223d8acd8152f47c68462483042577ba1
Diallo <adramane2707@gmail.com>
2026-03-12 14:19:18 +0100

    mon premier commit

---
+++ src/main/java/com/baticonnecte/baticonnecte/BaticonnecteApplication.java
@@ -0,0 +1,13 @@
+package com.baticonnecte.baticonnecte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaticonnecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaticonnecteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baticonnecte/baticonnecte/BaticonnecteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baticonnecte.baticonnecte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaticonnecteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
